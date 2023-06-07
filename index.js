1e0bd47a551591a9f70a2c028f0c1c128cb34558
Arthur-Noh <dkagod97@gmail.com>
2023-06-07 16:34:50 +0900

    feat: register api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import User from './models/user.js';
 
 const app = express();
 const port = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const userName = 'arthurNoh';
 const password = 'Dev79*42*27';
@@ -15,5 +19,17 @@ mongoose.connect(`mongodb+srv://${userName}:${password}@cluster0.sdvmndh.mongodb
 });
 
 app.get('/', (req, res) => res.send('Hello World'));
+
+// 회원가입
+app.post('/register', async (req, res) =>  {
+    const user = new User(req.body);
+
+    try {
+        const result = await user.save();
+        return res.status(200).json({ success: true });
+    } catch(error) {
+        return res.json({ success: false, error });
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
